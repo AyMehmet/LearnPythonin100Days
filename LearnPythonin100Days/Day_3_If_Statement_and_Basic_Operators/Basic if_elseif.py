5976e41c74cfeb6893af647d5056a451cc9a6f22
aymehmet <aymehmet1976@gmail.com>
2022-08-29 17:32:17 +0300

    day3

---
+++ LearnPythonin100Days/Day_3_If_Statement_and_Basic_Operators/Basic if_elseif.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 20
+
+if num1 < 50 and num2 < 50:
+    print("num1 and num2 are lower than 50")
+elif num1 < 20 or num2 < 30:
+    print("num1 is lower than 20 and num1: " + str(num1))
+    print(num2)
---
+++ LearnPythonin100Days/Day_3_If_Statement_and_Basic_Operators/Basic_if.py
@@ -0,0 +1,5 @@
+num = 10;
+if num < 10:
+    print("this number is lower than 10")
+else:
+    print("this number is higher than 10")
